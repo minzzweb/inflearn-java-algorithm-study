5662be4447c4651f830f16ccbf6961cf0ee096ea
minzzweb <minzzweb@gmail.com>
2023-09-05 15:24:37 +0900

    숫자만 추출

--- sec1_String/src/sec1/com/Sec1_09.java
+++ sec1_String/src/sec1/com/Sec1_09.java
@@ -1,10 +1,40 @@
 package sec1.com;
 
+import java.util.Scanner;
+
+//숫자만 추출 (48~57)
+//tge0a1h205er
+//자연수 1205
+
 public class Sec1_09 {
 
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
+	private int solution(String str) {
+		int answer = 0;
+		char[] s  = str.toCharArray();
+		for(char x :s) {
+			if(x>=48 && 57>=x) {
+				System.out.print(x);
+				answer = answer*10 +(x-48);
+				//System.out.println(answer);
+				System.out.println("");
+			}
+		}
+
+			return answer;
+			
+	}
+
+	
+	public static void main(String[] args) {
+		Sec1_09 T = new Sec1_09();
+		
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine(); 
+		
+
+		System.out.print(T.solution(str));
+
+	}
+
 
 }
