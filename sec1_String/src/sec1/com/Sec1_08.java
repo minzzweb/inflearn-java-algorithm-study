6da27af87a70df2415d1906661ccff26e4b42bc1
minzzweb <minzzweb@gmail.com>
2023-09-05 14:29:37 +0900

    팬린드롬 풀이

--- sec1_String/src/sec1/com/Sec1_08.java
+++ sec1_String/src/sec1/com/Sec1_08.java
@@ -1,10 +1,38 @@
 package sec1.com;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+//문제
+//유효한 팬린드롬 
+//앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팬린드롬이라고 한다. 
+//알파벳만 가지고 회문 검사 , 대소문자 구분하지 않음 
+//알파벳 이외의 문자들은 무시 
+//found7, time: study; Yduts; emit, 7Dnuof
 public class Sec1_08 {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
+
+	
+	private String solution(String str) {
+		String answer = "NO";
+
+		str = str.toUpperCase().replaceAll("[^A-Z]","");
+		String tmp = new StringBuilder(str).reverse().toString();
+		if(str.equals(tmp))answer="YES";
+	    
+			return answer;
+			
+	}
+
+	
+	public static void main(String[] args) {
+		Sec1_08 T = new Sec1_08();
+		
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine(); 
+		
+
+		System.out.print(T.solution(str));
+
+	}
 
 }
