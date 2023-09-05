421ad411a7536beeb5659bec1b5a5dbd54f46022
minzzweb <minzzweb@gmail.com>
2023-09-05 09:57:20 +0900

    대소문자 변환 풀이

--- sec1_String/src/sec1/com/Sec1_02.java
+++ sec1_String/src/sec1/com/Sec1_02.java
@@ -1,10 +1,33 @@
 package sec1.com;
 
-public class Sec1_02 {
+import java.util.Scanner;
 
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
+//문제 :
+//문자열을 입력받아 대문자는 소문자로
+//소문자는 대문자로 변환하여 출력
+//StuDY
+//sTUdy
+public class Sec1_02 {
+	
+    public void solution(String str1) {
+    	String answer ="";
+    	for(char c : str1.toCharArray()) {
+    		if(Character.isLowerCase(c)){
+    			answer += Character.toUpperCase(c);
+    		}else if(Character.isUpperCase(c)) {
+    			answer += Character.toLowerCase(c);
+    		}
+    	}
+    	
+    	System.out.print("answer : " + answer);
+    }
+
+	public static void main(String[] args) {
+		Sec1_02 sec = new Sec1_02();
+		
+		Scanner scanner = new Scanner(System.in);
+		String str1 = scanner.next();
+		sec.solution(str1);
+	}
 
 }
